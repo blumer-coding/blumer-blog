232b30425605b4ff976fd811b6f952e1532f3205
blumer <609961022@qq.com>
2019-08-21 05:24:17 +0000

    browser static cache

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="zh-CN">
+<html lang="{{ app()->getLocale() }}">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css"/>
 
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
-    <title>Blumer</title>
+    <title>@yield('title', 'LaraBBS') - Blumer 蓝默空间</title>
 
     <script type='text/javascript'>
         window.Laravel = <?php echo json_encode([
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blumer</title>
+    <title>@yield('title', 'LaraBBS') - Blumer 蓝默空间</title>
 
 </head>
 <body>
