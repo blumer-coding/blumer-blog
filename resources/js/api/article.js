a3b4e21d2c7faec4f2994c9ce1945c1977cac85d
blumer <609961022@qq.com>
2019-08-21 04:52:04 +0000

    Axios request

---
+++ resources/js/api/article.js
@@ -0,0 +1,33 @@
+/**
+ * Imports the Roast API URL from the config.
+ */
+import { ROAST_CONFIG } from '../config.js';
+
+export default {
+    /**
+     * GET /api/v1/articles
+     */
+    getarticles: function(){
+        return axios.get( ROAST_CONFIG.API_URL + '/articles' );
+    },
+    /**
+     * GET /api/v1/articles/{articleID}
+     */
+    getArticle: function( articleID ){
+        return axios.get( ROAST_CONFIG.API_URL + '/articles/' + articleID );
+    },
+    /**
+     * POST /api/v1/articles
+     */
+    postAddNewArticle: function( name, address, city, state, zip ){
+        return axios.post( ROAST_CONFIG.API_URL + '/articles',
+            {
+                name: name,
+                address: address,
+                city: city,
+                state: state,
+                zip: zip
+            }
+        );
+    }
+}
